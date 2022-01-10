d19592c00a87fddfacd610de3dbe54bc3f772331
asmae <echaib.asmae@gmail.com>
2022-01-10 14:11:16 +0000

    Controller

--- src/main/java/ma/ensa/controller/TransfertController.java
+++ src/main/java/ma/ensa/controller/TransfertController.java
@@ -1,9 +1,14 @@
 package ma.ensa.controller;
 
 import lombok.Data;
+import ma.ensa.converter.TransfertConverter;
+import ma.ensa.dto.TransfertDTO;
 import ma.ensa.service.TransfertService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("transfert")
@@ -11,6 +16,36 @@ import org.springframework.web.bind.annotation.RestController;
 public class TransfertController {
 
     final TransfertService transfertService;
+    final TransfertConverter transfertConverter;
+
+    @PostMapping("/")
+    public ResponseEntity<?> save(@RequestBody TransfertDTO transfertDTO) throws Exception {
+        if (transfertDTO == null)
+            return ResponseEntity.badRequest().body("The provided transfert is not valid");
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(transfertConverter.convertToDTO(transfertService.save(transfertConverter.convertToDM(transfertDTO))));
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<?> update(@RequestBody TransfertDTO transfertDTO) throws Exception {
+        if (transfertDTO == null)
+            return ResponseEntity.badRequest().body("The provided transfert is not valid");
+        return ResponseEntity
+                .ok().body(transfertConverter.convertToDTO(transfertService.update(transfertConverter.convertToDM(transfertDTO))));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) throws Exception {
+        if(id == null)
+            return ResponseEntity.badRequest().body("The provided transfert's id is not valid");
+        return ResponseEntity.ok().body("Transfert [" + transfertService.delete(id) + "] deleted successfully.");
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<TransfertDTO>> findAll() {
+        return ResponseEntity.ok().body(transfertConverter.convertToDTOs(transfertService.findAll()));
+    }
 
 
 }
