872b6767f35dfa967e160e5a18078e713418df06
ambou-97 <ambroise.tine@gmail.com>
2022-01-10 12:35:03 +0100

    Exceptions

---
+++ src/main/java/ma/ensa/exception/TransfertNotFoundException.java
@@ -0,0 +1,13 @@
+package ma.ensa.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+public class TransfertNotFoundException extends Exception{
+
+    private Long tid;
+}
