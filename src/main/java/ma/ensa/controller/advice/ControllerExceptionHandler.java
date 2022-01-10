aa08c280cc730717af6d2d037d734f06fbae9461
asmae <echaib.asmae@gmail.com>
2022-01-10 14:10:11 +0000

    Exceptions

---
+++ src/main/java/ma/ensa/controller/advice/ControllerExceptionHandler.java
@@ -0,0 +1,28 @@
+package ma.ensa.controller.advice;
+
+import ma.ensa.exception.TransfertDuplicatedException;
+import ma.ensa.exception.TransfertNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ExceptionHandler(TransfertNotFoundException.class)
+    private ResponseEntity<?> handleNotFoundException(TransfertNotFoundException exception){
+        String responseMessage = "The provided movie ["+exception.getId()+"] is nowhere to be found.";
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(responseMessage);
+    }
+
+    @ExceptionHandler(TransfertDuplicatedException.class)
+    private ResponseEntity<?> handleMovieDuplicatedException(TransfertDuplicatedException exception){
+        String responseMessage = "The provided movie ["+exception.getId()+"] is already existing.";
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(responseMessage);
+    }
+}
