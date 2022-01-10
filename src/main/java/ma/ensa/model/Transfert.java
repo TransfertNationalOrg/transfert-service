63b21955fa19e911383639c3f54ec7415f3c34ff
ambou-97 <ambroise.tine@gmail.com>
2022-01-10 12:49:20 +0100

    Transfert updates

--- src/main/java/ma/ensa/model/Transfert.java
+++ src/main/java/ma/ensa/model/Transfert.java
@@ -1,10 +1,6 @@
 package ma.ensa.model;
 
 import lombok.Data;
-import ma.ensa.model.enumer.ETAT;
-import ma.ensa.model.enumer.MODE;
-import ma.ensa.model.personne.Agent;
-import ma.ensa.model.personne.Personne;
 
 import javax.persistence.*;
 
@@ -17,19 +13,7 @@ public class Transfert {
 
     private Long ref;
     private double commission;
-    private ETAT etat;
     private boolean notification;//Si on l'émetteur souhaite recevoir un accusé de réception
-    @ManyToOne
-    @JoinColumn(name = "beneficiaire_ID")
-    private Personne beneficiaire;
-    @ManyToOne
-    @JoinColumn(name = "emetteur_ID")
-    private Personne emetteur;
-    @ManyToOne
-    @JoinColumn(name = "agent_id")
-    private Agent agent;
-    private MODE emission;
-    private MODE reception;
     private String motif;
 
 
