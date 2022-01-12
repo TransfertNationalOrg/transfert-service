b8862570ce3367b0da63f9f83ecd03f81f41435c
asmae <echaib.asmae@gmail.com>
2022-01-12 11:54:44 +0000

    Feign

--- src/main/java/ma/ensa/cmi/CompteBancaireFeign.java
+++ src/main/java/ma/ensa/cmi/CompteBancaireFeign.java
@@ -1,4 +1,20 @@
 package ma.ensa.cmi;
 
+import ma.ensa.client.ClientDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name="transfert-service")
+
 public interface CompteBancaireFeign {
+    //Get all compteBancaire from cmi-service
+    @GetMapping("compteBancaire/")
+    List<ClientDTO> findAll();
+
+    //Get a compteBancaire by id from cmi-service
+    @GetMapping("compteBancaire/{idCompteBancaire}")
+    CompteBancaireDTO getCompteBancaireById(@PathVariable("idCompteBancaire") Long id);
 }
