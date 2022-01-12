116e9e6bc77c39d42855f25b29979fd430d39acb
ambou-97 <ambroise.tine@gmail.com>
2022-01-12 14:28:51 +0100

    @Repository --> @RepositoryRestResource

--- src/main/java/ma/ensa/repository/TransfertRepository.java
+++ src/main/java/ma/ensa/repository/TransfertRepository.java
@@ -4,22 +4,23 @@ import ma.ensa.model.Transfert;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+@RepositoryRestResource
 public interface TransfertRepository extends JpaRepository<Transfert, Long> {
 
-    @Query("SELECT t from Transfert t where c.getIdClient=:x")
+    @Query("SELECT transfert from Transfert transfert where transfert.getIdClient()=:x")
     List<Transfert> findByClientId(@Param("x") Long clientId);
 
-    @Query("SELECT t from Transfert t where c.getIdAgent=:x")
+    @Query("SELECT transfert from Transfert transfert where transfert.getIdAgent()=:x")
     List<Transfert> findByAgentId(@Param("x") Long agentId);
 
-    @Query("SELECT t from Transfert t where c.getIdBeneficiaire=:x")
+    @Query("SELECT transfert from Transfert transfert where transfert.getIdBeneficiaire()=:x")
     List<Transfert> findByBeneficiaireId(@Param("x") Long beneficiaireId);
 
-    @Query("SELECT t from Transfert t where c.getIdClient=:x")
+    @Query("SELECT transfert from Transfert transfert where transfert.getIdClient()=:x")
     List<Transfert> findByClientBanqueId(@Param("x") Long clientId);
 }
