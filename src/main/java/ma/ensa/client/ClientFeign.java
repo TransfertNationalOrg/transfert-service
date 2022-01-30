4c7c1c549362adc11f8e81092b69fe33b16fda0e
ambou-97 <ambroise.tine@gmail.com>
2022-01-30 20:54:31 +0100

    adding @PutMapping("client/")

--- src/main/java/ma/ensa/client/ClientFeign.java
+++ src/main/java/ma/ensa/client/ClientFeign.java
@@ -3,16 +3,21 @@ package ma.ensa.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name="transfert-service")
+@FeignClient(name="client-service")
 public interface ClientFeign {
     //Get all clients from client-service
     @GetMapping("client/")
     List<ClientDTO> findAll();
 
     //Get a client by id from client-service
-    @GetMapping("client/{idClient}")
+    @GetMapping("client/{id}")
     ClientDTO getClientById(@PathVariable("id") Long id);
+
+    @PutMapping("client/")
+    ClientDTO update(@RequestBody ClientDTO clientDTO);
 }
